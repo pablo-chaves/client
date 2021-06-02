1164a7aabc05367b4cb75481ddc0b7157a6907d7
Sebastián Avila <75225819+sebastianomsk@users.noreply.github.com>
2021-06-02 15:16:23 -0300

    Update views.service.js

--- src/Services/views.service.js
+++ src/Services/views.service.js
@@ -4,6 +4,6 @@ const { REACT_APP_API_BASE_ENDPOINT } = process.env;
 
 // EDITAR UNA RESERVA
 export async function addViewsService(idPost, body) {
-    const endpoint = `http://localhost:3001/views/${idPost}`;
+    const endpoint = `${REACT_APP_API_BASE_ENDPOINT}/views/${idPost}`;
     return await axios.put(endpoint, body);
 }
